ef9da7283c58aa9f0b58c884d892abaf6c8760d0
akselhh <akselhh@ntnu.stud.no>
2023-10-10 14:54:31 +0200

    feat: Ordering by genre
    Added ordering by genre

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,26 @@ import BookCard from './components/BookCard'
 import { BookCardProps } from './types/BookCardProps'
 
 function App() {
-  const [books, setBooks] = useState([{ title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', year: 1954, rating: 4.5, genre: 'fantasy' },
-  { title: 'Harry Potter', author: 'J.K. Rowling', year: 1997, rating: 4.8, genre: 'fantasy' },
-  { title: 'The Hobbit', author: 'J.R.R. Tolkien', year: 1937, rating: 5, genre: 'fantasy' },
-  { title: 'The Little Prince', author: 'Antoine de Saint-Exupéry', year: 1943, rating: 4.3, genre: 'fiction' },
-  { title: 'Dream of the Red Chamber', author: 'Cao Xueqin', year: 1791, rating: 4.1, genre: 'romance' },
 
-  ])
+  const initialBooks = [
+    { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', year: 1954, rating: 4.5, genre: 'fantasy' },
+    { title: 'Harry Potter', author: 'J.K. Rowling', year: 1997, rating: 4.8, genre: 'fantasy' },
+    { title: 'The Hobbit', author: 'J.R.R. Tolkien', year: 1937, rating: 5, genre: 'fantasy' },
+    { title: 'The Little Prince', author: 'Antoine de Saint-Exupéry', year: 1943, rating: 4.3, genre: 'fiction' },
+    { title: 'Dream of the Red Chamber', author: 'Cao Xueqin', year: 1791, rating: 4.1, genre: 'romance' }
+  ];
+
+  const [books, setBooks] = useState(initialBooks)
+
+  const selectGenre = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const genre = e.target.value;
+    if (genre === 'all') {
+      setBooks(initialBooks);
+    } else {
+      const newBooks = initialBooks.filter((book) => book.genre === genre);
+      setBooks(newBooks);
+    }
+  }
 
   const orderBy = (property: keyof BookCardProps) => {
     const newBooks = [...books];
@@ -39,12 +52,24 @@ function App() {
 
   return (
     <>
+<div>
+  <label htmlFor="genreSelect">Select genre:</label>
+  <select id="genreSelect" onChange={selectGenre}>
+    <option value="all">All</option>
+    <option value="fantasy">Fantasy</option>
+    <option value="fiction">Fiction</option>
+    <option value="romance">Romance</option>
+  </select>
+</div>
+    <div>
+    <label htmlFor="orderBySelect">Order by:</label>
     <select onChange={(e) => orderBy(e.target.value as keyof BookCardProps)}>
         <option value="title">Title</option>
         <option value="author">Author</option>
         <option value="rating">Rating</option>
         <option value="year">Year</option>
       </select>
+    </div>
       <button onClick={handleReverse}>Reverse</button>
       <div className="book-card-container">
         {books.map((book) => (<BookCard title={book.title} author={book.author} year={book.year} rating={book.rating} genre={book.genre} />))}
