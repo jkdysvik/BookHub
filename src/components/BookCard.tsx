2cf3f081fb847c467c70faa4c9eded0918cf5df7
jkdysvik <jkd@live.no>
2023-10-11 11:39:29 +0200

    Styling of cards, select and button

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+//import React from 'react';
+import { AiFillStar } from 'react-icons/ai'
 import './BookCard.scss';
 import { BookCardProps } from '../types/BookCardProps';
 
@@ -6,7 +7,7 @@ const BookCard = ({ title, author, year, rating, genre }: BookCardProps) => {
     return (
         <div className="book-card">
             <h2 className="book-card.title">{title}</h2>
-            <div className="book-card.rating">{rating} </div>
+            <div className="book-card.rating">{rating} <AiFillStar/></div>
             <p className="book-card.author">{author}</p>
             <p className="book-card.year">{year}</p>
             <p className="book-card.genre">{genre}</p>
