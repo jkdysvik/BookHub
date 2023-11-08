2bdc9a9bc4167f4c322538bfd62c0f66e13e0766
Christian Li Sivertsen <christianlisiv@gmail.com>
2023-11-08 23:03:29 +0100

    Added buttons for decreasing and increasing offset

--- client/src/hooks/useGetBooks.ts
+++ client/src/hooks/useGetBooks.ts
@@ -4,16 +4,16 @@ import { useQuery } from "@tanstack/react-query";
 import { BookCardProps } from "../types/BookCardProps";
 
 
-export default function useGetBooks(chosenGenre?: string) {
+export default function useGetBooks(offset: number, chosenGenre: string) {
     const queryResult = useQuery({
-        queryKey: ["book", chosenGenre],
+        queryKey: ["book", { offset, chosenGenre }],
         queryFn: async () => {
             const data = await request<{ books: BookCardProps[] }>(
                 "http://localhost:4000/graphql",
                 GET_BOOKS,
                 {
                     limit: 10,
-                    offset: 0,
+                    offset: offset,
                     genre: chosenGenre,
                 },
             );
