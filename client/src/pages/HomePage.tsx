e7b2f67006dc659140ccd5e4bd2e783693d80712
Christian Li Sivertsen <christianlisiv@gmail.com>
2023-11-08 20:39:51 +0100

    Genre filtration working

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import '../App.css';
 import BookCard from '../components/BookCard';
 import { BookCardProps } from '../types/BookCardProps';
 import { useNavigate } from 'react-router';
-import { gql, useQuery } from '@apollo/client';
+import useGetBooks from '../hooks/useGetBooks';
 
-// the query to get all books
-const BOOKS = gql`
-query GetBooks($limit: Int, $offset: Int, $genre: String) {
-    books(limit: $limit, offset: $offset, genre: $genre) {
-      _id
-      title
-      author
-      year
-      rating
-      genre
-    }
-  }
-    `;
+
 
 interface Book extends BookCardProps {
     onClick: (id: string) => void;
@@ -27,35 +15,21 @@ function HomePage() {
     const navigate = useNavigate();
 
 
+
     const [books, setBooks] = useState<Book[]>([]);
-    const [chosenGenre, setChosenGenre] = useState<string>('Fantasy');
+    const [chosenGenre, setChosenGenre] = useState<string>('');
 
 
-    // the useQuery hook is used to make a query to the backend
-    const { loading, error, data } = useQuery(BOOKS, {
-        variables: {
-            limit: 10,
-            offset: 0,
-            genre: chosenGenre,
-        },
-    });
+
+    const { error, data, isLoading } = useGetBooks(chosenGenre);
 
     // set the books state to the data returned from the query  
-    useEffect(() => {
-        if (data) {
-            setBooks(data.books);
-        }
-    }, [data]);
 
-    useEffect(() => {
-        // search again with the new genre
-        // const newBooks = data.books.filter((book: { genre: string; }) => book.genre.toLowerCase() === chosenGenre);
-    }, [chosenGenre]);
 
     const selectGenre = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const genre = e.target.value;
         if (genre === 'all') {
-            setBooks(data.books);
+            // setBooks(data.books);
         } else {
             // const newBooks = data.books.filter((book: { genre: string; }) => book.genre.toLowerCase() === genre);
             // setBooks(newBooks);
@@ -92,7 +66,7 @@ function HomePage() {
         navigate('/project2/book/' + id);
     };
 
-    if (loading) return <p>Loading...</p>;
+    if (isLoading) return <p>Loading...</p>;
     if (error) return <p>Error {error.message}</p>;
 
     return (
@@ -100,12 +74,17 @@ function HomePage() {
             <div>
                 <label htmlFor="genreSelect">Select genre:</label>
                 <select id="genreSelect" onChange={selectGenre}>
-                    <option value="all">All</option>
-                    <option value="fantasy">Fantasy</option>
-                    <option value="fiction">Fiction</option>
-                    <option value="romance">Romance</option>
+                    <option value="">All</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Fiction">Fiction</option>
+                    <option value="Romance">Romance</option>
                 </select>
             </div>
+            <div onClick={() => console.log(data)}>asdfasdfasdfasdfasdf</div>
+            <div onClick={() => console.log(chosenGenre)}>print genre</div>
+
+
+
             <div>
                 <label htmlFor="orderBySelect">Order by:</label>
                 <select onChange={(e) => orderBy(e.target.value as keyof BookCardProps)}>
@@ -117,7 +96,7 @@ function HomePage() {
             </div>
             <button onClick={handleReverse}>Reverse</button>
             <div className="book-card-container">
-                {books.map((book) => (<BookCard onClick={handleCardClick} title={book.title} author={book.author} year={book.year} rating={book.rating} genre={book.genre} id={book._id} />))}
+                {data?.books.map((book) => (<BookCard onClick={handleCardClick} title={book.title} author={book.author} year={book.year} rating={book.rating} genre={book.genre} />))}
             </div>
         </>
     );
