b39102f86fd928cc6b5257e05bb675e768bf3c01
jkdysvik <jkd@live.no>
2023-11-08 18:43:44 +0100

    Fixed styling and navi

--- client/src/components/BookCard.tsx
+++ client/src/components/BookCard.tsx
@@ -8,11 +8,22 @@ const BookCard = ({ id, title, author, year, rating, genre, onClick }: BookCardP
         onClick(id);
     };
     
+    if(title.length > 20){
+        title = title.substring(0,20) + '...';
+    }
+    if(author.length > 20){
+        author = author.substring(0,20) + '...';
+    }
     return (
+        
         <div onClick={handleClick} className="book-card">
-            <h2 className="book-card.title">{title}</h2>
+            <div className="title-div">
+                <h2 className="book-card.title">{title}</h2>
+            </div>
             <div className="book-card.rating">{rating} <AiFillStar/></div>
-            <p className="book-card.author">{author}</p>
+            <div className="author-div">   
+                <p className="book-card.author">{author}</p>
+            </div>
             <p className="book-card.year">{year}</p>
             <p className="book-card.genre">{genre}</p>
         </div>
