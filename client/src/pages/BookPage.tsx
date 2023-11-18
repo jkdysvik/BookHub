0aafad6cd9e8fea1ea0d7dfd1ef007b51fb0bfe4
Christian Li Sivertsen <christianlisiv@gmail.com>
2023-11-18 15:50:23 +0100

    BookPage

--- client/src/pages/BookPage.tsx
+++ client/src/pages/BookPage.tsx
@@ -1,30 +1,72 @@
 import { useParams } from "react-router-dom";
 import useGetBook from "../hooks/useGetBook";
-
+import "./BookPage.scss";
+import { useState } from "react";
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 
 function BookPage() {
-  const { bookId } = useParams(); // Ensure this matches the URL parameter
+  const { bookId } = useParams();
 
   const { isLoading, error, data } = useGetBook(bookId);
+  const [showFullDescription, setShowFullDescription] = useState(false);
 
   if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error :</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   const book = data?.book;
-  return (
-    <div>
-      <h1>{book?.title}</h1>
-      <div id="bokinfo">
-        <p>Author: {book?.author}</p>
-        <p>Year: {book?.year}</p>
-        <p>Rating: {book?.rating}</p>
-        <p>Genre: {book?.genre}</p>
-        {book?.description && <p>Description: {book?.description}</p>}
-        {book?.pages && <p>Pages: {book?.pages}</p>}
-        {book?.language && <p>Language: {book?.language}</p>}
-      </div>
-    </div>
 
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
+
+  return (
+    <div className="bookPage-container">
+      <button className="bookpage-bookmark-button">
+        <BookmarkAddIcon />
+      </button>
+      <div className="bookPage-title">
+        {book?.title}
+      </div>
+      <div className="bookpage-info-container">
+        <div className="bookpage-info-field">
+          <div style={{ fontWeight: 'bold', marginRight: '5px' }}>Author:</div>
+          <div style={{ marginLeft: '5px' }}>{book?.author}</div>
+        </div>
+        <div className="bookpage-info-field">
+          <div style={{ fontWeight: 'bold', marginRight: '5px' }}>Year:</div>
+          <div style={{ marginLeft: '5px' }}>{book?.year}</div>
+        </div>
+        <div className="bookpage-info-field">
+          <div style={{ fontWeight: 'bold', marginRight: '5px' }}>Rating:</div>
+          <div style={{ marginLeft: '5px' }}>{book?.rating}</div>
+        </div>
+        <div className="bookpage-info-field">
+          <div style={{ fontWeight: 'bold', marginRight: '5px' }}>Genre:</div>
+          <div style={{ marginLeft: '5px' }}>{book?.genre}</div>
+        </div>
+        {book?.pages && <div className="bookpage-info-field">
+          <div style={{ fontWeight: 'bold', marginRight: '5px' }}>Pages:</div>
+          <div style={{ marginLeft: '5px' }}>{book?.pages}</div>
+        </div>}
+        {book?.language && <div className="bookpage-info-field">
+          <div style={{ fontWeight: 'bold', marginRight: '5px' }}>Language:</div>
+          <div style={{ marginLeft: '5px' }}>{book?.language}</div>
+        </div>}
+      </div>
+      <div style={{ fontWeight: 'bold', margin: '20px' }}>Description:</div>
+      <p>
+        {showFullDescription || (book?.description && book?.description.length < 400)
+          ? book?.description
+          : `${book?.description.substring(0, 400)}...`}
+      </p>
+      {
+        book?.description && book.description.length > 400 && (
+          <button className="bookpage-readmore-button" onClick={toggleDescription}>
+            {showFullDescription ? "Read Less" : "Read More"}
+          </button>
+        )
+      }
+    </div>
   );
 }
 
