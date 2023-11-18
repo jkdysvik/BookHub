e3d87527d6b831cfddc9cba3d6d455cc138a3ac2
Christian Li Sivertsen <christianlisiv@gmail.com>
2023-11-18 19:38:37 +0100

    Changed to mui star

--- client/src/components/BookCard.tsx
+++ client/src/components/BookCard.tsx
@@ -1,5 +1,5 @@
 //import React from 'react';
-import { AiFillStar } from 'react-icons/ai'
+import StarIcon from '@mui/icons-material/Star';
 import './BookCard.scss';
 import { BookCardProps } from '../types/BookCardProps';
 
@@ -7,21 +7,23 @@ const BookCard = ({ _id, title, author, year, rating, genre, onClick }: BookCard
     const handleClick = () => {
         onClick(_id);
     };
-    
-    if(title.length > 20){
-        title = title.substring(0,20) + '...';
+
+    if (title.length > 20) {
+        title = title.substring(0, 20) + '...';
     }
-    if(author.length > 20){
-        author = author.substring(0,20) + '...';
+    if (author.length > 20) {
+        author = author.substring(0, 20) + '...';
     }
     return (
-        
+
         <div onClick={handleClick} className="book-card">
             <div className="title-div">
                 <h2 className="book-card.title">{title}</h2>
             </div>
-            <div className="book-card.rating">{rating} <AiFillStar/></div>
-            <div className="author-div">   
+            <div className="book-card.rating">{rating}
+                <StarIcon style={{ fontSize: 'large', marginLeft: '5px', color: '#35633b' }} />
+            </div>
+            <div className="author-div">
                 <p className="book-card.author">{author}</p>
             </div>
             <p className="book-card.year">{year}</p>
