dc2c15bfb64f9958332afcc1b9c8aca2ce3ebbc3
Christian Li Sivertsen <christianlisiv@gmail.com>
2023-11-18 15:50:10 +0100

    Css navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,8 @@
-
 import "./Navbar.css";
 import { ChangeEvent, useState } from "react";
 import logo from "../assets/logo.png";
 import { useNavigate } from "react-router";
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Navbar() {
     const [query, setQuery] = useState<string>("");
@@ -15,24 +15,23 @@ export default function Navbar() {
 
     const handleSearchClick = () => {
         if (query.trim() !== "") {
-          navigate(`/project2/search?query=${query}`);
+            navigate(`/project2/search?query=${query}`);
         }
-      };
-    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter" && query.trim() !== "") {
-        navigate(`/project2/search?query=${query}`);
-    }
     };
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter" && query.trim() !== "") {
+            navigate(`/project2/search?query=${query}`);
+        }
+    };
+
     const handleLogoClick = () => {
         navigate("/project2/");
     }
 
-
     return (
         <nav className="navbar-container">
             <img onClick={handleLogoClick} className="logo" src={logo} alt="logo" />
-            <button className="navbar-button">Categories</button>
-            <button className="navbar-button">Readlist</button>
             <div className="search-bar">
                 <input
                     className="search-input"
@@ -41,9 +40,14 @@ export default function Navbar() {
                     value={query}
                     onChange={handleInputChange}
                     onKeyUp={handleKeyPress}
+
                 />
-                <button className="navbar-button" onClick={handleSearchClick}>Search</button>
-            </div >
+                <button className="navbar-button" onClick={handleSearchClick}>
+                    <SearchIcon style={{ height: '20px' }} />
+                    Search
+                </button>
+            </div>
+            <button className="navbar-button">Readlist</button>
         </nav>
     );
 }
