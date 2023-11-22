cffdeeed56aea2df092fb7c40d95e3e015098ea2
jkdysvik <jkd@live.no>
2023-11-22 18:19:34 +0100

    Test for HomePage BookCard

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: { "process.env": {} },
   plugins: [react()],
+  // TODO: Remove later
   base: "/project2",
-})
+
+  test: {
+    testTimeout: 10000,
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.js",
+    environmentMatchGlobs: [
+      // all tests in tests/dom will run in jsdom
+      ["tests/dom/**", "jsdom"],
+      // all tests in tests/ with .edge.test.ts will run in edge-runtime
+      ["**/*.edge.test.ts", "edge-runtime"],
+      // ...
+    ],
+  },
+});
